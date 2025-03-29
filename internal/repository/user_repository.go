15467cf4e1582c588995df57f88d7f25ccccbdb7
shivajee98 <ultimateintimater@gmail.com>
2025-03-29 16:39:07 +0530

    user repo code completed

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"github.com/shivajee98/aamishrit/internal/model"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(user *model.User) error
+	GetUserByPhone(phone string) (*model.User, error)
+	UpdateUser(user *model.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func InitUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) CreateUser(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) GetUserByPhone(phone string) (*model.User, error) {
+	var user model.User
+	err := r.db.Where("phone = ?", phone).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) UpdateUser(user *model.User) error {
+	return r.db.Save(user).Error
+}
