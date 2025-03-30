0ac09a80da5b2af76c3d3f12318a3e9ff9ec8058
shivajee98 <ultimateintimater@gmail.com>
2025-03-30 23:30:59 +0530

    pack up

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,1 +1,9 @@
 package services
+
+import "github.com/shivajee98/aamishrit/internal/model"
+
+type UserService interface {
+	RegisterUser(user *model.User) error
+	LoginUser(phone, password string) (*model.User, error)
+
+}
