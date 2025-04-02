6cc503331332d7c3525e4051517ac85161a789a4
shivajee98 <ultimateintimater@gmail.com>
2025-04-02 11:29:21 +0530

    main.go setup completed for products route and also tested the apis 🎎

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"log"
 
-	"github.com/clerk/clerk-sdk-go/v2"
-	"github.com/clerk/clerk-sdk-go/v2/jwks"
 	"github.com/gofiber/fiber/v2"
 	"github.com/shivajee98/aamishrit/internal/config"
 	"github.com/shivajee98/aamishrit/internal/db"
 	"github.com/shivajee98/aamishrit/internal/handlers"
-	"github.com/shivajee98/aamishrit/internal/middleware"
+	"github.com/shivajee98/aamishrit/internal/repository"
 	"github.com/shivajee98/aamishrit/internal/routes"
+	"github.com/shivajee98/aamishrit/internal/services"
 	"github.com/shivajee98/aamishrit/pkg/utils"
 )
 
@@ -23,18 +22,12 @@ func main() {
 	// Connect to DB
 	dbConn, err := db.Connect(cfg.DatabaseURL)
 
-	// Initialising Clerk and Storing JWK
+	// setup
+	productRepo := repository.InitProductRepository(dbConn)
+	productService := services.InitProductService(productRepo)
+	productHandler := handlers.InitProductHandler(productService)
 
-	clerkConfig := &clerk.ClientConfig{}
-	clerkConfig.Key = clerk.String(cfg.ClerkSecretKey)
-	jwkClient := jwks.NewClient(clerkConfig)
-	jwkStore := middleware.NewJWKStore()
-
-	// Initialising Handler with DB
-	handler := handlers.Provide(dbConn)
-
-	// Register Routes
-	routes.Setup(app, jwkClient, jwkStore, handler)
+	routes.Setup(app, productHandler)
 
 	utils.CheckError("Database Connection Failed!", err)
 
